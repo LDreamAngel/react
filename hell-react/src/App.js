27c85b23a3fb2352fed4ecb773b623ae5157b94e
lmq <LiMq@rgbvr.com>
2019-01-11 20:12:14 +0800

    父传子及传递数据类型的规范

--- hell-react/src/App.js
+++ hell-react/src/App.js
@@ -5,15 +5,23 @@ import Header from './components/Header';
 import Home from './components/Home';
 
 //用ES6的写法继承上面react引入的Component
+//App为父组件，Header和Home为子组件，Header和Home之间又为兄弟组件
 class App extends Component {
   render() {
+    const user={
+      name:"LMQ",
+      hobbies:["跑步","阅读"]
+    }
     return (
       // 因为class为保留字，所以这块儿用className
       <div className="container">
        {/* 自定义组件的使用 */}
        <div className="row">
-            <div className="col-xs-1 col-xs-offset-11">  
-              <Header/>
+            <div className="col-xs-1 col-xs-offset-11"> 
+              {/* 将父组件中的数据传递给子组件  */}
+              <Header name={"LLHY"} age={12} obj={user}>
+                 <p>我是App父组件传给Header子组件的子节点</p>
+              </Header>
             </div>
          </div>
 
--- hell-react/src/components/Header.js
+++ hell-react/src/components/Header.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+// 引入数据类型
+import PropTypes from 'prop-types';
 
 export default class Header extends Component {
   render() {
+    // 打印父组件传递过来的值
+    console.log(this.props)
     return (
       <div className="container">
          <div className="row">
             <div className="col-xs-1 col-xs-offset-11">  
               <h1>Header</h1>
+              <p>他的名字是{this.props.name},他今年{this.props.age}岁</p>
+              <h4>爱好</h4>
+              <ul>
+                {/* 循环创建 */}
+                {this.props.obj.hobbies.map((hobby,i)=><li key={i}>{hobby}</li>)}
+              </ul>
+              {/* 接收父组件传过来的子节点 */}
+              <p>{this.props.children}</p>
             </div>
          </div>
       </div>
@@ -14,3 +26,10 @@ export default class Header extends Component {
   }
 }
 
+//定义数据类型,当数据类型不正确的时候回报错
+Header.propTypes={
+   name:PropTypes.string,
+   age:PropTypes.number,
+   obj:PropTypes.object,
+   children:PropTypes.element.isRequired,
+}
