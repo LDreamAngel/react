e5e6514458250359650e0c4041954fbf851ce207
lmq <LiMq@rgbvr.com>
2019-01-11 18:47:12 +0800

    react多组件化的开发

--- hell-react/src/App.js
+++ hell-react/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//组件引用
+import Header from './components/Header';
+import Home from './components/Home';
 
 //用ES6的写法继承上面react引入的Component
 class App extends Component {
@@ -8,28 +10,25 @@ class App extends Component {
     return (
       // 因为class为保留字，所以这块儿用className
       <div className="container">
+       {/* 自定义组件的使用 */}
+       <div className="row">
+            <div className="col-xs-1 col-xs-offset-11">  
+              <Header/>
+            </div>
+         </div>
+
          <div className="row">
             <div className="col-xs-1 col-xs-offset-11">  
               <h1>Hello React !!!</h1>
             </div>
          </div>
+        
+         <div className="row">
+            <div className="col-xs-1 col-xs-offset-11">  
+              <Home/>
+            </div>
+         </div>
       </div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     {/* <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p> */}
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
     );
   }
 }
